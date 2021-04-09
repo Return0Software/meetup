4ddecd518ba7388273587ee90684fc9190893431
Joseph Martinsen <joseph@martinsen.com>
2021-04-08 20:40:00 -0500

    add cov

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  collectCoverageFrom: ["<rootDir>/pages/**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   transform: {
